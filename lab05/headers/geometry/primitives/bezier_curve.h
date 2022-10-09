aa10b87157717b83427ea04751130721153c0681
Elisa <handlessdude@gmail.com>
2022-10-09 22:56:33 +0300

    fully working composite cubic bezier drawing

--- lab05/headers/geometry/primitives/bezier_curve.h
+++ lab05/headers/geometry/primitives/bezier_curve.h
@@ -7,8 +7,8 @@ class BezierCurve : public Primitive
 {
 	float _step = 0.02;
 	void draw_skeleton(ImDrawList* draw_list, const ImVec2& offset);
-	ImVec2& nextBt3(const std::deque<ImVec2*>& points, const float& t);
-	void draw_curve3(ImDrawList* draw_list, const std::deque<ImVec2*>& points, const ImVec2& offset);
+	ImVec2& nextBt3(const std::deque<ImVec2>& points, const float& t);
+	void draw_curve3(ImDrawList* draw_list, const std::deque<ImVec2>& points, const ImVec2& offset);
 public:
 	//BezierCurve(ImVector<ImVec2>* points, const ImU32& color, const float& thickness);
 	BezierCurve(const ImU32& color, const float& thickness);
