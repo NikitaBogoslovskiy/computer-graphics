a412775d5c2861539447078af53a4763a37c13c2
Elisa <handlessdude@gmail.com>
2022-11-20 20:47:05 +0300

    WHYYYYYYYPLANE EQUATIONS CRASH

--- lab05/headers/geometry/methods/clipper.h
+++ lab05/headers/geometry/methods/clipper.h
@@ -9,8 +9,8 @@ class Clipper
 {
 	static constexpr const byte RegionCodes[] = { 1, 2, 4, 8, 16, 32 }; // left, right, bottom, top, near, far
 	static constexpr const byte RegionIndex[] = { 0, 0, 1, 1, 2, 2 };
-	static constexpr const char RegionSign[] = { -1, 1, -1, 1, -1, 1 };
-	static constexpr const char Coef[] = { 1, 1, 1, 1, 0, 1 };
+	//static constexpr const char RegionSign[] = { -1, 1, -1, 1, -1, 1 };
+	//static constexpr const char Coef[] = { 1, 1, 1, 1, 0, 1 };
 	static constexpr const char Planes[] = { -1, 1, -1, 1, 0, 1 }; // left, right, bottom, top, near, far
 
 	static Eigen::Vector4f interpolate(const Eigen::Vector4f& p0, const Eigen::Vector4f& p1, const float& t) {
@@ -61,11 +61,12 @@ public:
 		Eigen::Vector4f p;
 		bool accept = false;
 		size_t pointToAdjust = 0;
-		if (code[0]) {}
-		else { pointToAdjust = 1; }
+		//if (code[0]) {}
+		//else { pointToAdjust = 1; }
 
 		while (true) {
-			if (!(code[0] | code[1])) {
+			//if (!(code[0] | code[1])) {
+			if ((code[0]==0) && (code[1]==0)) {
 				accept = true;
 				break;
 			}
@@ -76,14 +77,20 @@ public:
 			else { pointToAdjust = 1; }
 			//static constexpr const byte RegionCodes[] = { 1, 2, 4, 8, 16, 32 }; // left, right, bottom, top, near, far
 			//static constexpr const char RegionSign[] = { -1, 1, -1, 1, -1, 1 };
+			//static constexpr const byte RegionIndex[] = { 0, 0, 1, 1, 2, 2 };
 			for (size_t r = 0; r < 6; r++) { // order of edges based on priority
 				if (!(code[pointToAdjust] & RegionCodes[r])) continue;
+				std::cout << "before: " << (*points[pointToAdjust])(0) << " " << (*points[pointToAdjust])(1) << " " << (*points[pointToAdjust])(2) << " " << (*points[pointToAdjust])(3) << " " << std::endl;
 				auto ri = RegionIndex[r];
-				float d1 = Coef[ri] * RegionSign[r] * (*points[0])(3) + RegionSign[r] * (*points[0])(ri);
-				float d2 = Coef[ri] * RegionSign[r] * (*points[1])(3) + RegionSign[r] * (*points[1])(ri);
-				*points[pointToAdjust] = interpolate((*points[0]), (*points[1]), d1 / (d2 - d1));
-				(*points[pointToAdjust])(3) = abs((*points[pointToAdjust])(RegionIndex[r]));
+				float plane = (*points[pointToAdjust])(3) * Planes[r];
+				float t = (plane - (*points[0])(ri)) / ((*points[1])(ri) - (*points[0])(ri));
+				//float d1 = Coef[ri] * RegionSign[r] * (*points[0])(3) + RegionSign[r] * (*points[0])(ri);
+				//float d2 = Coef[ri] * RegionSign[r] * (*points[1])(3) + RegionSign[r] * (*points[1])(ri);
+				//*points[pointToAdjust] = interpolate((*points[0]), (*points[1]), d1 / (d2 - d1));
+				(*points[pointToAdjust]) = interpolate((*points[0]), (*points[1]), t);
+				(*points[pointToAdjust])(3) = abs((*points[pointToAdjust])(ri));
 				code[pointToAdjust] = getRegionCode(*points[pointToAdjust]);
+				std::cout << "after adjusting: " << (*points[pointToAdjust])(0) << " " << (*points[pointToAdjust])(1) << " " << (*points[pointToAdjust])(2) << " " << (*points[pointToAdjust])(3) << " " << std::endl << std::endl;
 				break;
 			}
 		}
